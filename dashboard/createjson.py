1674d0735347576e422f15e6f90b27039da28e7e
priya299 <vppriya9@gmail.com>
2016-04-05 21:12:47 +0530

    Updated createjson.py file

--- dashboard/createjson.py
+++ dashboard/createjson.py
@@ -16,6 +16,16 @@ import sys
 msg_ids = []
 msg_json = []
 def create_json(perceval_out, mbox_files):
+    """
+    This function parses the perceval output, which
+    is a json file 'out.json' to get the message ids.
+    Then threading algorithm is run over the mbox files
+    to group messages belonging to same thread
+    and it is written to 'new.json' file.
+
+    :param perceval_out: perceval output, which is a json file.
+    :param mbox_files : mbox file of xen-devel list
+    """
     with open(perceval_out) as f:
         for line in f:
             while True:
@@ -33,14 +43,14 @@ def create_json(perceval_out, mbox_files):
         for key, value in messages.iteritems():
             for k in msg_json:
                 if key == k['Message-ID'].strip('<>'):
-                    k['property']=key
+                    k['property'] = key
                     json.dump(k, f, ensure_ascii=True, indent=4)
 
             if value:
-                for key in value:
+                for i in value:
                     for j in msg_json:
-                        if key == j['Message-ID'].strip('<>'):
-                            j['property'] = key
+                        if i == j['Message-ID'].strip('<>'):
+                            j['property'] = i
                             json.dump(j, f, ensure_ascii=True, indent=4)
                             break
         f.close()
