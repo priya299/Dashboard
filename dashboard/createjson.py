5cd4ddf415c14bd1bbd0857643ca52e5a9d1886e
priya299 <vppriya9@gmail.com>
2016-04-06 17:28:01 +0530

    Added argparse

--- dashboard/createjson.py
+++ dashboard/createjson.py
@@ -3,24 +3,31 @@ Author: Priya V
 
 """
 
-import json
-import jwzthreading_r as th
 import sys
+import json
+import argparse
+
+import jwzthreading_r as th
 import perceval.backends as backend
+
 
 msg_ids = []
 msg_json = []
+
+
 def create_json(mbox_files, output_file):
     """
     This function uses perceval to parse the mailing list archieve
     and gets the message ids. Then threading algorithm is run over
     the mbox files to group messages belonging to same thread and
-    it is written to 'new.json' file.
+    it is written to the output file.
 
     :param mbox_files : mbox file of xen-devel list
     :param output_file : output file name
 
     """
+
+
     mbox_parser = backend.mbox.MBox(
 	origin="http://lists.xenproject.org/archives/html/mbox/"+mbox_files,
 	dirpath='.'
@@ -47,5 +54,9 @@ def create_json(mbox_files, output_file):
         f.close()
 
 if __name__ == "__main__":
-    create_json(sys.argv[1],sys.argv[2])
-    print("new.json file has been created")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--mbox",required=True,help="Give the name of the mbox file to be parsed")
+    parser.add_argument("--output", required=True, help="Name of the output json file")
+    args = parser.parse_args()
+    create_json(args.mbox,args.output)
+    print("Output file %s created"%args.output)
