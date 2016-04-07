0d09e91a5b973870ed54ab9a5ef96dd901d9913e
priya299 <vppriya9@gmail.com>
2016-04-07 17:41:12 +0530

    Added perceval logging

--- dashboard/createjson.py
+++ dashboard/createjson.py
@@ -24,6 +24,7 @@
 import sys
 import json
 import argparse
+import logging
 
 import jwzthreading_r as th
 import perceval.backends as backend
@@ -39,16 +40,12 @@ def create_json(mbox_files, output_file):
     and gets the message ids. Then threading algorithm is run over
     the mbox files to group messages belonging to same thread and
     it is written to the output file.
-
-    :param mbox_files : mbox file of xen-devel list
-    :param output_file : output file name
-
+    :param mbox_files: mbox file of xen-devel list
+    :param output_file: output file name
     """
-
-
     mbox_parser = backend.mbox.MBox(
-	origin="http://lists.xenproject.org/archives/html/mbox/"+mbox_files,
-	dirpath='.'
+            origin="http://lists.xenproject.org/archives/html/mbox/"+mbox_files,
+            dirpath='.'
     )
     perceval_out = mbox_parser.fetch()
     for item in perceval_out:
@@ -58,17 +55,24 @@ def create_json(mbox_files, output_file):
     with open(output_file,'a') as f:
         for key, value in messages.items():
             for k in msg_json:
-                if key == k['Message-ID'].strip('<>'):
-                    k['property'] = key
-                    json.dump(k, f, ensure_ascii=True, indent=4)
+                try:
+                    if key == k['Message-ID'].strip('<>'):
+                        k['property'] = key
+                        json.dump(k, f, ensure_ascii=True, indent=4)
+                except KeyError:
+                        logging.debug('Received an email without the correct Message Id %s', str(k))
 
             if value:
                 for i in value:
                     for j in msg_json:
-                        if i == j['Message-ID'].strip('<>'):
-                            j['property'] = key
-                            json.dump(j, f, ensure_ascii=True, indent=4)
-                            break
+                        try:
+                            if i == j['Message-ID'].strip('<>'):
+                                j['property'] = key
+                                json.dump(j, f, ensure_ascii=True, indent=4)
+                                break
+                        except KeyError as e:
+                            logging.debug('Received an email without the correct Message Id')
+
         f.close()
 
 if __name__ == "__main__":
@@ -76,5 +80,7 @@ if __name__ == "__main__":
     parser.add_argument("--mbox",required=True,help="Give the name of the mbox file to be parsed")
     parser.add_argument("--output", required=True, help="Name of the output json file")
     args = parser.parse_args()
+    logging.basicConfig(filename='perceval_mbox_parse.log', level=logging.DEBUG)
     create_json(args.mbox,args.output)
     print("Output file %s created"%args.output)
+
