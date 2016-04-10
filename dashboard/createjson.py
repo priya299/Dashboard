491ffc2cf03d41af16e3adffd80a4d824e76eed5
priya299 <vppriya9@gmail.com>
2016-04-10 16:30:52 +0530

    Updated createjson file

--- dashboard/createjson.py
+++ dashboard/createjson.py
@@ -34,54 +34,65 @@ msg_ids = []
 msg_json = []
 
 
-def create_json(mbox_files, output_file):
-    """
-    This function uses perceval to parse the mailing list archieve
-    and gets the message ids. Then threading algorithm is run over
-    the mbox files to group messages belonging to same thread and
-    it is written to the output file.
-    :param mbox_files: mbox file of xen-devel list
-    :param output_file: output file name
-    """
-    mbox_parser = backend.mbox.MBox(
-            origin="http://lists.xenproject.org/archives/html/mbox/"+mbox_files,
-            dirpath='.'
-    )
-    perceval_out = mbox_parser.fetch()
-    for item in perceval_out:
-        msg_json.append(item)
+class MboxParser:
 
-    messages = th.message_details(mbox_files)
-    with open(output_file,'a') as f:
-        for key, value in messages.items():
-            for k in msg_json:
-                try:
-                    if key == k['Message-ID'].strip('<>'):
-                        k['property'] = key
-                        json.dump(k, f, ensure_ascii=True, indent=4)
-                        break
-                except KeyError:
-                        logging.debug('Received an email without the correct Message Id %s', str(k))
+    def getmbox(self, mbox_files):
+        mbox_parser = backend.mbox.MBox(
+                origin = mbox_files,
+                dirpath='.'
+        )
+        return mbox_parser.fetch()
 
-            if value:
-                for i in value:
-                    for j in msg_json:
-                        try:
-                            if i == j['Message-ID'].strip('<>'):
-                                j['property'] = key
-                                json.dump(j, f, ensure_ascii=True, indent=4)
-                                break
-                        except KeyError as e:
-                            logging.debug('Received an email without the correct Message Id')
+    def create_json(self, mbox_files, output_file):
+        """
+        This function uses perceval to parse the mailing list archieve
+        and gets the message ids. Then threading algorithm is run over
+        the mbox files to group messages belonging to same thread and
+        it is written to the output file.
+        :param mbox_files: mbox file of xen-devel list
+        :param output_file: output file name
+        """
+        percevalout = self.getmbox(mbox_files)
+        for item in percevalout:
+            msg_json.append(item)
 
-        f.close()
+        messages = th.message_details(mbox_files)
+        with open(output_file,'a') as f:
+            for key, value in messages.items():
+                for k in msg_json:
+                    try:
+                        if key == k['Message-ID'].strip('<>'):
+                            k['property'] = key
+                            json.dump(k, f, ensure_ascii=True, indent=4)
+                            break
+                    except KeyError:
+                            logging.debug('Received an email without the correct Message Id %s', str(k))
 
-if __name__ == "__main__":
+                if value:
+                    for i in value:
+                        for j in msg_json:
+                            try:
+                                if i == j['Message-ID'].strip('<>'):
+                                    j['property'] = key
+                                    json.dump(j, f, ensure_ascii=True, indent=4)
+                                    break
+                            except KeyError as e:
+                                logging.debug('Received an email without the correct Message Id')
+
+            f.close()
+
+
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--mbox",required=True,help="Give the name of the mbox file to be parsed")
     parser.add_argument("--output", required=True, help="Name of the output json file")
     args = parser.parse_args()
     logging.basicConfig(filename='perceval_mbox_parse.log', level=logging.DEBUG)
-    create_json(args.mbox,args.output)
+    mparser = MboxParser()
+    mparser.create_json(args.mbox,args.output)
     print("Output file %s created"%args.output)
+
+if __name__ == "__main__":
+        main()
+
 
