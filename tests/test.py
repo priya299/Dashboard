9f1cdb6cbc8ae65227c6bccf9baf567387fb2e60
priya299 <vppriya9@gmail.com>
2016-06-03 14:42:48 +0530

    Testing Code

--- tests/test.py
+++ tests/test.py
@@ -1,19 +1,47 @@
+from collections import Counter
+import json
+import sys
+sys.path.insert(0,'../')
+import jwzthreading_r as th
 import unittest
+import os
+
+sys.path.insert(0,'../')
 from mboxparser import MboxParser
 
-class test_mbox_mails(unittest.TestCase):
+class Test_Mbox_Mails(unittest.TestCase):
 
 	def setUp(self):
+		try:
+			os.remove('testoutput.json')
+		except OSError:
+			pass
+
 		self.mparser = MboxParser()
 
 	def test(self):
-		mbox = self.mparser.mboxparser('xen-devel-2016-03', 'testoutput.json')
+		value_count=0
+		original_count=0
+		mbox = self.mparser.create_json('xen-devel-2016-05', 'testoutput.json', file=True)
+		messages = th.message_details('xen-devel-2016-05',file=True)
+		for key,value in messages.items():
+				value_count=0
+				original_count = len(value)
+				with open('testoutput.json') as f:
+					for line in f:
+						while True:
+							try:
+								jfile=json.loads(line)
+								break
+							except ValueError:
+								line+=next(f)
+						if jfile['property'] == key:
+							value_count = value_count + 1
 
+				self.assertEquals(original_count+1,value_count,"Equal")
 
 	def tearDown(self):
 		del self.mparser
 
 if __name__ == '__main__':
 	unittest.main()
-
-
